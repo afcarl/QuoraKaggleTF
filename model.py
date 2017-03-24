f0734919e28876b67f9cd90c644a1503909c854f
tal <tal@smartscribe.io>
2017-03-24 18:38:35 +0300

    Added convolutions before rnn

--- model.py
+++ model.py
@@ -72,16 +72,15 @@ class BasicQuoraModel():
                     tf.summary.histogram(var.name + '/gradient', grad)
 
     @staticmethod
-    def prepare_sentance(sent,lengths,matrix,cell):
-        embedded =tf.nn.embedding_lookup(matrix,sent)
-        outputs, s1_sate = tf.nn.bidirectional_dynamic_rnn(cell_fw=cell, cell_bw=cell, inputs=embedded, dtype=tf.float32,
+    def prepare_sentance(sent,lengths,cell,):
+        outputs, s1_sate = tf.nn.bidirectional_dynamic_rnn(cell_fw=cell, cell_bw=cell, inputs=sent, dtype=tf.float32,
                                                               sequence_length=lengths)
         return tf.concat(outputs,2)
 
     @staticmethod
     def inference(s1,s2,l1,l2):
         with tf.variable_scope("model",initializer=xavier_initializer()):
-            s1_lstmed, s2_lstmed = BasicQuoraModel.rnn_sentances( l1, l2, s1, s2)
+            l1,l2,s1_lstmed, s2_lstmed = BasicQuoraModel.rnn_sentances( l1, l2, s1, s2)
             difs_state = BasicQuoraModel.rnn_compare_sentances(l1, l2, s1_lstmed, s2_lstmed)
             logits = tf.contrib.layers.linear(difs_state,num_outputs=2)
         return logits
@@ -97,19 +96,42 @@ class BasicQuoraModel():
                                                                   dtype=tf.float32, sequence_length=dif_len)
         difs_state = tf.concat([difs_state[0].c, difs_state[0].c], 1)
         return difs_state
+    @staticmethod
+    def convolve_embedded_sentances(l1, l2, s1, s2,embedding_size,stride=2):
+        batch_size,seqeunce_length,_ = s1.shape
+        filters_1 = tf.get_variable(name="filters_1",shape=[stride*2,embedding_size,2*embedding_size])
+        filters_2 = tf.get_variable(name="filters_2", shape=[stride*2, 2*embedding_size, 4 * embedding_size])
+        s1_conv = tf.nn.conv1d(s1,filters_1,stride,"VALID")
+        s1_conv = tf.nn.conv1d(s1_conv, filters_2, stride, "VALID")
+
+        s2_conv = tf.nn.conv1d(s2, filters_1, stride, "VALID")
+        s2_conv = tf.nn.conv1d(s2_conv, filters_2, stride, "VALID")
+
+        l1 = tf.clip_by_value(l1//(stride**2),1,s1_conv.shape[1])
+        l2 = tf.clip_by_value(l2//(stride**2),1,s2_conv.shape[1])
+
+        return l1,l2, s1_conv,s2_conv
 
     @staticmethod
     def rnn_sentances( l1, l2, s1, s2):
         with tf.variable_scope("inference", ) as scope:
-            with tf.device("/cpu:0"):
-                embedding_size = int(np.sqrt(FLAGS.vocab_size) + 1)
-                embedding_matrix = tf.get_variable("embedding_matrix", shape=[FLAGS.vocab_size, embedding_size],
-                                                   dtype=tf.float32)
+            embedding_size, s1_embeded, s2_embeded = BasicQuoraModel.embed_sentances(s1, s2)
+            l1,l2,s1_conv,s2_conv = BasicQuoraModel.convolve_embedded_sentances(l1,l2,s1_embeded,s2_embeded,embedding_size)
             cell = tf.contrib.rnn.LayerNormBasicLSTMCell(num_units=FLAGS.hidden1)
-            s1_lstmed = BasicQuoraModel.prepare_sentance(s1, l1, embedding_matrix, cell=cell)
+            s1_lstmed = BasicQuoraModel.prepare_sentance(s1_conv, l1, cell=cell,)
             scope.reuse_variables()
-            s2_lstmed = BasicQuoraModel.prepare_sentance(s2, l2, embedding_matrix, cell=cell)
-        return s1_lstmed, s2_lstmed
+            s2_lstmed = BasicQuoraModel.prepare_sentance(s2_conv, l2,  cell=cell,)
+        return l1,l2,s1_lstmed, s2_lstmed
+
+    @staticmethod
+    def embed_sentances(s1, s2):
+        with tf.device("/cpu:0"):
+            embedding_size = int(np.sqrt(FLAGS.vocab_size) + 1)
+            embedding_matrix = tf.get_variable("embedding_matrix", shape=[FLAGS.vocab_size, embedding_size],
+                                               dtype=tf.float32)
+            s1_embeded = tf.nn.embedding_lookup(embedding_matrix, s1)
+            s2_embeded = tf.nn.embedding_lookup(embedding_matrix, s2)
+        return embedding_size, s1_embeded, s2_embeded
 
     @staticmethod
     def loss(logits,targets):
